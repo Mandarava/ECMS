e9b66448d340c103733747581ebca89545f88bf7
Mandarava <569045298@qq.com>
2017-02-02 19:40:51 +0800

    lucene

---
+++ src/main/java/com/finance/lucene/Searcher.java
@@ -0,0 +1,54 @@
+package com.finance.lucene;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Created by zt on 2017/1/22.
+ */
+public class Searcher {
+
+    public static void search(String indexDir, String keyword) {
+        try {
+            Directory dir = FSDirectory.open(Paths.get(indexDir));
+            IndexReader reader = DirectoryReader.open(dir);
+            IndexSearcher searcher = new IndexSearcher(reader);
+            Analyzer analyzer = new StandardAnalyzer();
+            QueryParser parser = new QueryParser("contents", analyzer);
+            Query query = parser.parse(keyword);
+            long start = System.currentTimeMillis();
+            TopDocs topDocs = searcher.search(query, 10);
+            long end = System.currentTimeMillis();
+            System.out.println("匹配 " + keyword + "总共花费 " + (end - start) + "毫秒" + topDocs.totalHits + "个记录");
+            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                Document document = searcher.doc(scoreDoc.doc);
+                System.out.print(document.get("fullPath"));
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String indexDir = "G:\\workspace\\lucene";
+        String keyword = "deliberately";
+        search(indexDir, keyword);
+    }
+}
