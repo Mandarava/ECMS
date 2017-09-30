1bb505e360001ba7d01d3a375fc576dac91ac4b0
Mandarava <569045298@qq.com>
2017-09-30 21:23:16 +0800

    BaseResponse.java

---
+++ src/main/java/com/finance/model/vo/BaseResponse.java
@@ -0,0 +1,39 @@
+package com.finance.model.vo;
+
+import java.io.Serializable;
+
+/**
+ * Created by zt 2017/9/30 12:32
+ */
+public class BaseResponse<T extends Serializable> implements Serializable {
+
+    private int code;
+
+    private String message;
+
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
